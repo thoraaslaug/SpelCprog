75a183f6b30ecad5c8acffabae18b2e09abfc1ba
Thora Magnusdottir <70522885+thoraaslaug@users.noreply.github.com>
2021-12-13 15:54:39 +0100

    musik rullar yay

--- grupp20/src/System.cpp
+++ grupp20/src/System.cpp
@@ -1,15 +1,19 @@
 #include "System.h"
 #include <SDL2/SDL.h>
 
+#include <iostream>
+
 namespace grupp20
 {
-	std::string resPath = "../resources/";
+	std::string resPath = "./resources/";
+
     System::System() {
 	    SDL_Init(SDL_INIT_EVERYTHING); //starta allt
 	    window = SDL_CreateWindow("PROJEKT 20", 10, 10, 700, 500, 0); //fönster + storlek + fönsternamn
 	    renderer = SDL_CreateRenderer(window, -1, 0); //rendrera innehållet i fönstret
 	    Mix_OpenAudio(20050, AUDIO_S16SYS, 2, 4096);
 	    music = Mix_LoadWAV( (resPath + "sounds/bgMusic.wav").c_str() ); //sätter igång musiken vid start
+		//std::cout << music << std::endl;
 	    Mix_PlayChannel(-1, music, -1); //sätter loopar och L/R audio
     }
 
