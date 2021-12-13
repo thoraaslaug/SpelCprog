774b9ab39c9b8e1101781de1f638418a60d96bb8
yaouOps <augustjansson@outlook.com>
2021-12-13 10:29:36 +0100

    projektstruktur

---
+++ grupp20/src/main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+
+
+//using namespace std;
+int main(int argc, char* argv[]){
+    std::string greetings = "Hello World!";
+    std::cout << greetings << std::endl;
+
+    return 0;
+}
